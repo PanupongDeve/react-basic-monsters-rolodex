6ea5bc2e01d993ca7b4068d7657acac055f29cb3
Panupong Chamsomboon <panupong.dev.web@gmail.com>
2022-02-01 03:55:17 +0000

    [ADD] - add events change text

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import { Component } from 'react/cjs/react.production.min';
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Panupong'
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello my name is Panupong Chamsomboon
+            {this.state.string}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.setState({ string: 'Hello BOBA'})} >Change Text</button>
         </header>
       </div>
     );
